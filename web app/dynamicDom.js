e2581f7c772005a3ad08b85aeefccaf3a18fcc05
bipinoli <bipinoli90@gmail.com>
2019-07-10 22:14:45 +0545

    dynamic creation of list element from existing structure working properly

--- web app/dynamicDom.js
+++ web app/dynamicDom.js
@@ -1,3 +1,118 @@
+
+// build the dom structure from the structure object
+function buildStructure(domStructure) {
+
+    // build the dom structure
+    let root = document.createElement(domStructure.tag);
+    root.className = domStructure.class;
+    if (domStructure.hasOwnProperty("properties")) {
+        for (let i=0; i<domStructure.properties.length; i++) {
+            let propName = Object.getOwnPropertyNames(domStructure.properties[i])[0];
+            let propValue = domStructure.properties[i][propName];
+            root[propName] = propValue;
+        }
+    }
+
+    if (domStructure.hasOwnProperty("children")) {
+        // build the structures of children and assign them
+        // as children of the root
+        let children = [];
+        for (let i=0; i<domStructure.children.length; i++) {
+            children.push(buildStructure(domStructure.children[i]));
+        } 
+
+        for (let i=0; i<domStructure.children.length; i++) {
+            root.appendChild(children[i]);
+        }
+    }
+
+    return root;
+}
+
+
+
+
+// create list item dynamically from the given data
+function createListItem(data) {
+    data = {
+        itemName: "RTX 2080 Ti" + (Math.floor(Math.random()*100)),
+        creationDate: "10 May 2019",
+        creationTime: "11:21 PM",
+        domain: "Daraz",
+        currentPrice: "Rs. 1200",
+        desiredPrice: "Rs. 1000",
+        sourceUrl: "https://www.daraz.com.np/products/lace-up-solid-oxford-shoes-for-women-i100800793-s1021298274.html?spm=a2a0e.11779170.flashSale.3.287d2d2b5Oey1n&search=1&mp=1&c=fs"
+    };
+
+    html = buildStructure(ITEM_STRUCTURE);
+
+    // completely traverse the sub-tree 
+    // to find all the desendents
+    let desendents = [];
+
+    let stack = [html];
+    while (stack.length != 0) {
+        let node = stack.pop();
+        for (let j=0; j<node.childNodes.length; j++) {
+            desendents.push(node.childNodes[j]);
+            stack.push(node.childNodes[j]);
+        }
+    }
+
+    for (let j=0; j<desendents.length; j++) {
+
+        let child = desendents[j];
+        if (child.className && child.className == "item-name") {
+            let txt = document.createTextNode(data.itemName);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "item-date") {
+            let txt = document.createTextNode(data.creationDate);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "item-time") {
+            let txt = document.createTextNode(data.creationTime);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "item-source-domain") {
+            let txt = document.createTextNode(data.domain);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "item-current-price") {
+            let txt = document.createTextNode(data.currentPrice);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "current-price") {
+            let txt = document.createTextNode(data.currentPrice);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "desired-price") {
+            let txt = document.createTextNode(data.desiredPrice);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "desired-price-label") {
+            let txt = document.createTextNode(data.desiredPrice);
+            child.appendChild(txt);
+        }
+        if (child.className && child.className == "source-url") {
+            let txt = document.createTextNode(data.sourceUrl);
+            child.appendChild(txt);
+        }
+    }
+
+    return html;
+}
+
+
+function addListItem() {
+    document.getElementsByClassName("list-view")[0].appendChild(createListItem({}));
+
+    // rerun the interactions javascript
+    // to make new elements interactable
+    makeListInteractive();
+}
+
+
 const ITEM_STRUCTURE = {
     "tag": "div",
     "class": "list-item",
@@ -14,7 +129,7 @@ const ITEM_STRUCTURE = {
                     "tag": "div",
                     "class": "item-name",
                     "properties": [
-                        {"contenteditable": "true"},
+                        {"contentEditable": "true"},
                         {"spellcheck": "false"}
                     ]
                 },
@@ -57,6 +172,12 @@ const ITEM_STRUCTURE = {
                                 {
                                     "tag": "h3",
                                     "class": "detail-price",
+                                    "children": [
+                                        {
+                                            "tag": "span",
+                                            "class": "current-price",
+                                        }
+                                    ]
                                 },
                                 {
                                     "tag": "h3",
@@ -74,7 +195,7 @@ const ITEM_STRUCTURE = {
                             "tag": "div",
                             "class": "desired-price-label",
                             "properties": [
-                                {"contenteditable": "true"},
+                                {"contentEditable": "true"},
                                 {"spellcheck": "false"}
                             ]
                         },
@@ -111,58 +232,3 @@ const ITEM_STRUCTURE = {
     ]
 };
 
-
-
-
-
-function buildStructure(domStructure) {
-
-    // build the dom structure bottom up
-    
-}
-
-
-
-
-
-
-
-
-// create list item dynamically from the given data
-function createListItem(data) {
-    let list_item =  document.createElement("div");
-    list_item.className = "list-item";
-    
-    list_item = createDomNode(list_item, ["div", "div"], ["list-top", "list-details"]);
-    let list_top = list_item.children[0];
-    let list_details = list_item.children[1];
-
-    list_top = createDomNode(list_top, ["div", "div", "div", "div", "div"], ["color-line", "item-name", "item-date-time", "item-source-domain", "item-current-price"]);
-    let color_line = list_top.children[0];
-    let item_name = list_top.children[1];
-    let item_date_time = list_top.children[2];
-    let item_source_domain = list_top.children[3];
-}
-
-
-function createDomNode(parent, childrenTags, childrenClasses) {
-    for (let i=0; i<childrenTags.length; i++) {
-        let child = document.createElement(childrenTags[i]);
-        child.className = childrenClasses[i];
-        parent.appendChild(child);
-    }
-    return parent;
-}
-
-function assignChildren(parent, children) {
-    // remove existing children
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-
-    for (let i=0; i<children.length; i++) {
-        parent.appendChild(children[i]);
-    }
-
-    return parent;
-}
--- web app/interactions.js
+++ web app/interactions.js
@@ -1,125 +1,159 @@
-// assign random colors to the list items
-const COLORS = ["#E16367", "#F0C930", "#275BAE"];
-let list_items = document.getElementsByClassName("list-item");
 
-for (let i=0; i<list_items.length; i++) {
-    let list_item = list_items[i];
+// it holds the click event listeners
+// it is done so that when the listeners are to be removed
+// same reference of listener can be provided
+// also,
+// listeners in this list are bind to their respective object
+let clickEventListeners = [];
 
-    // completely traverse the sub-tree 
-    // to find all the desendents
-    let desendents = [];
 
-    let stack = [list_item];
-    while (stack.length != 0) {
-        let node = stack.pop();
-        for (let j=0; j<node.childNodes.length; j++) {
-            desendents.push(node.childNodes[j]);
-            stack.push(node.childNodes[j]);
+function makeListInteractive() {
+    
+    // assign random colors to the list items
+    const COLORS = ["#E16367", "#F0C930", "#275BAE"];
+    let list_items = document.getElementsByClassName("list-item");
+    
+    for (let i=0; i<list_items.length; i++) {
+        let list_item = list_items[i];
+    
+        // completely traverse the sub-tree 
+        // to find all the desendents
+        let desendents = [];
+    
+        let stack = [list_item];
+        while (stack.length != 0) {
+            let node = stack.pop();
+            for (let j=0; j<node.childNodes.length; j++) {
+                desendents.push(node.childNodes[j]);
+                stack.push(node.childNodes[j]);
+            }
+        }
+    
+        // color nodes according to their classes
+        const color = COLORS[Math.floor(Math.random()*COLORS.length)];
+        for (let j=0; j<desendents.length; j++) {
+    
+            let child = desendents[j];
+    
+    
+            if (child.className && child.className == "color-line")
+                child.style.backgroundColor = color;
+            if (child.className && child.className == "item-name")
+                child.style.color = color;
+            if (child.className && child.className == "item-date-time")
+                child.style.color = color;
+            if (child.className && child.className == "item-source-domain")
+                child.style.color = color;
+            if (child.className && child.className == "item-current-price")
+                child.style.backgroundColor = color;
+            if (child.className && child.className == "list-details")
+                child.style.backgroundColor = color;
+            if (child.className && child.className == "desired-price-label")
+                child.style.color = color;
+            if (child.className && child.className == "delete-btn")
+                child.style.color = color;
         }
     }
+    
+    
+    
+    
+    // handle click on item head
+    let item_heads = document.getElementsByClassName("list-top");
+    
+    for (let i=0; i<item_heads.length; i++) {
+        let item_head = item_heads[i];
+    
+        // create event listener if doesn't exists
+        if (clickEventListeners.length <= i) {
+            
+            clickEventListeners.push(
+                function clickHandler() {
+                    item_head = this;
+                    let details = item_head.parentNode.children[1];
+            
+                    console.log("display: " + details.style.display);
 
-    // color nodes according to their classes
-    const color = COLORS[Math.floor(Math.random()*COLORS.length)];
-    for (let j=0; j<desendents.length; j++) {
+                    if (!details.style.display) {
+                        details.style.display = "flex";
+                        console.log("no display");
+                    }
+                    else if (details.style.display == "none") {
+                        details.style.display = "flex";
+                    } 
+                    else if (details.style.display == "flex") {
+                        details.style.display = "none";
+                    }
+            });
 
-        let child = desendents[j];
+            clickEventListeners[i].bind(item_head);
+        }
 
+        // for some reason the first click had to be double click
+        // but with this line of code that was fixed
+        // item_head.parentNode.children[1].style.display = "none";
 
-        console.log(child.className);
-        if (child.className && child.className == "color-line")
-            child.style.backgroundColor = color;
-        if (child.className && child.className == "item-name")
-            child.style.color = color;
-        if (child.className && child.className == "item-date-time")
-            child.style.color = color;
-        if (child.className && child.className == "item-source-domain")
-            child.style.color = color;
-        if (child.className && child.className == "item-current-price")
-            child.style.backgroundColor = color;
-        if (child.className && child.className == "list-details")
-            child.style.backgroundColor = color;
-        if (child.className && child.className == "desired-price-label")
-            child.style.color = color;
-        if (child.className && child.className == "delete-btn")
-            child.style.color = color;
+        // remove if click handler already exists
+        item_head.removeEventListener("click", clickEventListeners[i], true);
+        item_head.addEventListener("click", clickEventListeners[i], true);
     }
+    
+    
+    
+    
+    
+    // handle change in item name
+    let item_names = document.getElementsByClassName("item-name");
+    let input_changed = [];
+    for (let i=0; i<item_names.length; i++) {
+        input_changed.push(false);
+    }
+    
+    
+    for (let i=0; i<item_names.length; i++) {
+        let item_name = item_names[i];
+    
+        item_name.addEventListener("input", function () {
+            input_changed[i] = true;
+        });
+    
+        item_name.addEventListener("focusout", function () {
+            if (input_changed[i]) {
+                console.log(i, item_name.textContent);
+            }
+            input_changed[i] = false;
+        })
+    }
+    
+    
+    
+    
+    
+    // handle change in desired price
+    let desired_prices = document.getElementsByClassName("desired-price-label");
+    let price_changed = [];
+    for (let i=0; i<desired_prices.length; i++) {
+        price_changed.push(false);
+    }
+    
+    
+    for (let i=0; i<desired_prices.length; i++) {
+        let desired_price = desired_prices[i];
+    
+        desired_price.addEventListener("input", function () {
+            price_changed[i] = true;
+            desired_price.parentNode.children[0].children[1].children[0].textContent = desired_price.textContent;
+        });
+    
+        desired_price.addEventListener("focusout", function () {
+            if (price_changed[i]) {
+                console.log(i, desired_price.textContent);
+            }
+            price_changed[i] = false;
+        })
+    }
+
+
 }
 
-
-
-
-// handle click on item head
-let item_heads = document.getElementsByClassName("list-top");
-
-for (let i=0; i<item_heads.length; i++) {
-    let item_head = item_heads[i];
-
-    // for some reason the first click had to be double click
-    // but with this line of code that was fixed
-    item_head.parentNode.children[1].style.display = "none";
-
-    item_head.addEventListener("click", function () {
-        let details = item_head.parentNode.children[1];
-
-        if (details.style.display === "none") {
-            details.style.display = "flex";
-        } else {
-            details.style.display = "none";
-        }
-    });
-}
-
-
-
-
-
-// handle change in item name
-let item_names = document.getElementsByClassName("item-name");
-let input_changed = [];
-for (let i=0; i<item_names.length; i++) {
-    input_changed.push(false);
-}
-
-
-for (let i=0; i<item_names.length; i++) {
-    let item_name = item_names[i];
-
-    item_name.addEventListener("input", function () {
-        input_changed[i] = true;
-    });
-
-    item_name.addEventListener("focusout", function () {
-        if (input_changed[i]) {
-            console.log(i, item_name.textContent);
-        }
-        input_changed[i] = false;
-    })
-}
-
-
-
-
-
-// handle change in desired price
-let desired_prices = document.getElementsByClassName("desired-price-label");
-let price_changed = [];
-for (let i=0; i<desired_prices.length; i++) {
-    price_changed.push(false);
-}
-
-
-for (let i=0; i<desired_prices.length; i++) {
-    let desired_price = desired_prices[i];
-
-    desired_price.addEventListener("input", function () {
-        price_changed[i] = true;
-        desired_price.parentNode.children[0].children[1].children[0].textContent = desired_price.textContent;
-    });
-
-    desired_price.addEventListener("focusout", function () {
-        if (price_changed[i]) {
-            console.log(i, desired_price.textContent);
-        }
-        price_changed[i] = false;
-    })
-}
+makeListInteractive();
